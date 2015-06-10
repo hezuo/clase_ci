573181061908195ca897953726c55dee2e7bcda9
Cesar Pillihuaman <cesarpillihuaman@gmail.com>
2015-06-10 10:43:50 -0500

    avance en clase 01

--- application/controllers/PaginaController.php
+++ application/controllers/PaginaController.php
@@ -14,13 +14,38 @@ class PaginaController extends CI_Controller {
         $this->load->helper('url');
         $this->load->view('paginas/homepage');
     }
+    
     public function contacto()
     {
         $this->load->helper('html');
         $this->load->helper('url');
         $this->load->helper('form');
-        $this->load->view('shared/header');
-        $this->load->view('paginas/contacto');
+        
+        $data["remitente"] = array( 'name' => 'remitente',
+        		'id'          => 'remitente',
+        		'class'       => 'form-control',
+        		'maxlength'   => '100',
+        		'size'        => '50');
+        
+        $data["correo"] = array( 'name' => 'correo',
+        		'id'          => 'correo',
+        		'class'       => 'form-control',
+        		'maxlength'   => '150');
+        
+        $data["mensaje"] = array( 'name' => 'mensaje',
+        		'id'          => 'mensaje',
+        		'class'       => 'form-control');
+        $data["ciudades"] = array( 'lima' => 'Lima',
+        		'arequipa'          => 'Arequipa',
+        		'cuzco'       => 'Cuzco',
+        		'piura'       => 'Piura',
+        		'trujillo'       => 'Trujillo'
+        );
+        
+        
+       $this->load->view('shared/header');
+        $this->load->view('paginas/contacto', $data);
         $this->load->view('shared/footer');
+         
     }
 }
--- application/views/paginas/contacto.php
+++ application/views/paginas/contacto.php
@@ -1,19 +1,16 @@
 <?php echo validation_errors(); ?>
 
 <?php echo form_open('procesar/formulario'); ?>
+<?php echo form_label('Remitente', 'remitente'); ?>
+<?php echo form_input($remitente); ?>
+<?php echo form_label('Correo', 'correo'); ?>
+<?php echo form_input($correo); ?>
+<?php echo form_label('Seleccione su ciudad', 'ciudades'); ?>
+<?php echo form_dropdown('ciudades',$ciudades, array(), 'class="form-control"'); ?>
+<?php echo form_label('Ingrese su mensaje', 'smensaje'); ?>
+<?php echo form_textarea($mensaje); ?>
+<?php echo form_submit('mysubmit', 'Enviar mensaje','class="btn btn-success"'); ?>
 
-<h5>Username</h5>
-<input type="text" name="username" value="" size="50" />
 
-<h5>Password</h5>
-<input type="text" name="password" value="" size="50" />
-
-<h5>Password Confirm</h5>
-<input type="text" name="passconf" value="" size="50" />
-
-<h5>Email Address</h5>
-<input type="text" name="email" value="" size="50" />
-
-<div><input type="submit" value="Submit" /></div>
 
 <?php echo form_close(); ?>
